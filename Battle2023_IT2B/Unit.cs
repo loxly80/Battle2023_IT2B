92cfcd28940b6f006a304eec1803f0a5569edcda
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-09-11 11:00:52 +0200

    Přidejte soubory projektu.

---
+++ Battle2023_IT2B/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle2023_IT2B
+{
+  public class Unit
+  {
+    private int hp;
+    public int HP
+    {
+      get => hp;
+      set => hp = Math.Max(Math.Min(value, 200), 50);
+    }
+
+    private int dmg;
+    public int DMG
+    {
+      get
+      {
+        return dmg;
+      }
+      set
+      {
+        dmg = Math.Max(Math.Min(value, 20), 5);
+      }
+    }
+
+    public Unit(int hp, int dmg)
+    {
+
+    }
+
+   
+  }
+}
