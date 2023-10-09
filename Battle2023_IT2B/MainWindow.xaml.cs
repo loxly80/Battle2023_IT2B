7e5e58f7d63675c5e1429a9897e1704666476c32
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-10-09 10:41:12 +0200

    po hodině

--- Battle2023_IT2B/MainWindow.xaml.cs
+++ Battle2023_IT2B/MainWindow.xaml.cs
@@ -21,34 +21,54 @@ namespace Battle2023_IT2B
   /// </summary>
   public partial class MainWindow : Window
   {
+    Unit hrac1;
+
     public MainWindow()
     {
-      Unit hrac1 = new Unit(500, 10);
+      hrac1 = new Unit(500, 10);
 
       InitializeComponent();
+      this.Loaded += MainWindow_Loaded;
+      this.SizeChanged += MainWindow_SizeChanged;
+    }
 
+    private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+      DrawGame();
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      DrawGame();
+    }
+
+    private void DrawGame()
+    {
+      canvas.Children.Clear();
+      DrawGround();
       DrawUnit(hrac1);
+    }
+
+    private void DrawGround()
+    {
+      Rectangle rectangle = new Rectangle();
+      rectangle.Width = canvas.ActualWidth;
+      rectangle.Height = 60;
+      rectangle.Fill = Brushes.Green;
+      Canvas.SetLeft(rectangle, 0);
+      Canvas.SetBottom(rectangle, 0);
+      canvas.Children.Add(rectangle);
     }
 
     private void DrawUnit(Unit unit)
     {
       Image image = new Image();
-      image.HorizontalAlignment = HorizontalAlignment.Stretch;
-      image.VerticalAlignment = VerticalAlignment.Stretch;
-      image.Source = GetImage("Img/archer.png");
+      image.Height = 100;
+      image.Width = 100; 
+      image.Source = new BitmapImage(new Uri("Img/archer.png",UriKind.Relative));
       Canvas.SetLeft(image, 20);
-      Canvas.SetTop(image, 150 );
+      Canvas.SetBottom(image, 25);
       canvas.Children.Add(image);
-    }
-
-    private BitmapFrame GetImage(string imagePath)
-    {
-      BitmapFrame bitmap;
-      using(FileStream fs = new FileStream(imagePath,FileMode.Open, FileAccess.Read, FileShare.Read))
-      {
-        bitmap = BitmapFrame.Create(fs);
-      }
-      return bitmap;
-    }
+    }    
   }
 }
